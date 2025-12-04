8ee163993466ba2aa222e5cc2d455eb6cf116142
Andy Wang <therainruffy@yahoo.com>
2025-12-03 20:49:00 -0500

    fix 5-image issues

--- ui/gui.py
+++ ui/gui.py
@@ -262,15 +262,17 @@ class CollageWindow(tk.Toplevel):
 			px, py, pw, ph = self.scale_coords(wcol, sum(h_right[:i]), w-wcol, h_right[i])
 			tile = Tile(self.canvas, px, py, pw, ph, i+2, self.check_export)
 			self.tiles.append(tile)
-		# Drag bars for left column
+		# Drag bars for left column (local list)
+		self.drag_bars_left = []
 		px, py, pw, ph = self.scale_coords(0, h_left[0]-5, wcol, 10)
-		bar_left = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy: self.resize_tiles_column(dy, h_left, 2, 0, wcol))
-		self.drag_bars.append(bar_left)
-		# Drag bars for right column
+		bar_left = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy: self.resize_tiles_column(dy, h_left, 2, 0, wcol, 0, 'left'))
+		self.drag_bars_left.append(bar_left)
+		# Drag bars for right column (local list)
+		self.drag_bars_right = []
 		for i in range(2):
 			px, py, pw, ph = self.scale_coords(wcol, h_right[i]-5+sum(h_right[:i]), w-wcol, 10)
-			bar = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy, idx=i: self.resize_tiles_column(dy, h_right, 3, 2, w-wcol, idx))
-			self.drag_bars.append(bar)
+			bar = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy, idx=i: self.resize_tiles_column(dy, h_right, 3, 2, w-wcol, idx, 'right'))
+			self.drag_bars_right.append(bar)
 
 	def init_five_three_two(self):
 		w, h = self.output_size
@@ -287,17 +289,19 @@ class CollageWindow(tk.Toplevel):
 			px, py, pw, ph = self.scale_coords(wcol, sum(h_right[:i]), w-wcol, h_right[i])
 			tile = Tile(self.canvas, px, py, pw, ph, i+3, self.check_export)
 			self.tiles.append(tile)
-		# Drag bars for left column
+		# Drag bars for left column (local list)
+		self.drag_bars_left = []
 		for i in range(2):
 			px, py, pw, ph = self.scale_coords(0, h_left[i]-5+sum(h_left[:i]), wcol, 10)
-			bar = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy, idx=i: self.resize_tiles_column(dy, h_left, 3, 0, wcol, idx))
-			self.drag_bars.append(bar)
-		# Drag bar for right column
+			bar = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy, idx=i: self.resize_tiles_column(dy, h_left, 3, 0, wcol, idx, 'left'))
+			self.drag_bars_left.append(bar)
+		# Drag bar for right column (local list)
+		self.drag_bars_right = []
 		px, py, pw, ph = self.scale_coords(wcol, h_right[0]-5, w-wcol, 10)
-		bar_right = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy: self.resize_tiles_column(dy, h_right, 2, 3, w-wcol))
-		self.drag_bars.append(bar_right)
+		bar_right = VerticalDragBar(self.canvas, px, py, pw, ph, lambda dy: self.resize_tiles_column(dy, h_right, 2, 3, w-wcol, 0, 'right'))
+		self.drag_bars_right.append(bar_right)
 
-	def resize_tiles_column(self, dy, heights, n, tile_offset, col_width, idx=0):
+	def resize_tiles_column(self, dy, heights, n, tile_offset, col_width, idx=0, which='left'):
 		prev_h = self.preview_size[1]
 		out_h = self.output_size[1]
 		scale = out_h / prev_h
@@ -313,14 +317,20 @@ class CollageWindow(tk.Toplevel):
 		for i in range(n):
 			px, py, pw, ph = self.scale_coords((0 if tile_offset==0 else col_width), y_positions[i], col_width, heights[i])
 			self.tiles[tile_offset+i].update_size(px, py, pw, ph)
-		# Update drag bars
+		# Update drag bars for the correct column only
 		if n == 2:
 			px, py, pw, ph = self.scale_coords((0 if tile_offset==0 else col_width), heights[0]-5, col_width, 10)
-			self.drag_bars[-1].update_position(px, py, pw, ph)
+			if which == 'left':
+				self.drag_bars_left[0].update_position(px, py, pw, ph)
+			else:
+				self.drag_bars_right[0].update_position(px, py, pw, ph)
 		else:
 			for i in range(n-1):
 				px, py, pw, ph = self.scale_coords((0 if tile_offset==0 else col_width), heights[i]-5+y_positions[i], col_width, 10)
-				self.drag_bars[tile_offset+i].update_position(px, py, pw, ph)
+				if which == 'left':
+					self.drag_bars_left[i].update_position(px, py, pw, ph)
+				else:
+					self.drag_bars_right[i].update_position(px, py, pw, ph)
 
 	def init_three_vertical(self):
 		w, h = self.output_size
@@ -364,20 +374,127 @@ class CollageWindow(tk.Toplevel):
 			px, py, pw, ph = self.scale_coords(0, y_positions[i]+heights[i]-5, w, 10)
 			self.drag_bars[i].update_position(px, py, pw, ph)
 	def redraw_tiles(self):
-		# Redraw all tiles and drag bars on resize
+		# Redraw all tiles and drag bars on resize, layout-aware
 		w, h = self.output_size
-		heights = [self.tiles[i].h for i in range(3)]
-		# Map preview heights to output heights
-		for i in range(3):
-			_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i].h)
-			heights[i] = ph
-		y_positions = [0, heights[0], heights[0]+heights[1]]
-		for i in range(3):
-			px, py, pw, ph = self.scale_coords(0, y_positions[i], w, heights[i])
-			self.tiles[i].update_size(px, py, pw, ph)
-		for i in range(2):
-			px, py, pw, ph = self.scale_coords(0, y_positions[i]+heights[i]-5, w, 10)
-			self.drag_bars[i].update_position(px, py, pw, ph)
+		layout = getattr(self, 'current_layout', None)
+		if layout is None:
+			# Try to infer layout from tile count
+			n = len(self.tiles)
+			if n == 3:
+				layout = '3-vertical'
+			elif n == 4:
+				layout = '4-vertical' if len(getattr(self, 'drag_bars', [])) == 3 else '4-grid'
+			elif n == 5:
+				layout = '5-2-3' if hasattr(self, 'drag_bars_left') else '5-3-2'
+			else:
+				return
+		if layout == '3-vertical':
+			heights = [self.tiles[i].h for i in range(3)]
+			for i in range(3):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i].h)
+				heights[i] = ph
+			y_positions = [0, heights[0], heights[0]+heights[1]]
+			for i in range(3):
+				px, py, pw, ph = self.scale_coords(0, y_positions[i], w, heights[i])
+				self.tiles[i].update_size(px, py, pw, ph)
+			for i in range(2):
+				px, py, pw, ph = self.scale_coords(0, y_positions[i]+heights[i]-5, w, 10)
+				self.drag_bars[i].update_position(px, py, pw, ph)
+		elif layout == '4-vertical':
+			n = 4
+			heights = [self.tiles[i].h for i in range(n)]
+			for i in range(n):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i].h)
+				heights[i] = ph
+			y_positions = [0]
+			for i in range(n-1):
+				y_positions.append(y_positions[-1] + heights[i])
+			for i in range(n):
+				px, py, pw, ph = self.scale_coords(0, y_positions[i], w, heights[i])
+				self.tiles[i].update_size(px, py, pw, ph)
+			for i in range(n-1):
+				px, py, pw, ph = self.scale_coords(0, y_positions[i]+heights[i]-5, w, 10)
+				self.drag_bars[i].update_position(px, py, pw, ph)
+		elif layout == '4-grid':
+			# 2x2 grid
+			widths = [self.tiles[0].w, self.tiles[1].w]
+			heights = [self.tiles[0].h, self.tiles[2].h]
+			for i in range(2):
+				_, _, pw, _ = self.inv_scale_coords(0, 0, self.tiles[i].w, 0)
+				widths[i] = pw
+			for i in range(2):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i*2].h)
+				heights[i] = ph
+			positions = [
+				(0, 0, widths[0], heights[0]),
+				(widths[0], 0, widths[1], heights[0]),
+				(0, heights[0], widths[0], heights[1]),
+				(widths[0], heights[0], widths[1], heights[1])
+			]
+			for i, (x, y, tw, th) in enumerate(positions):
+				px, py, pw, ph = self.scale_coords(x, y, tw, th)
+				self.tiles[i].update_size(px, py, pw, ph)
+			# Drag bars: 0 = vertical, 1 = horizontal
+			px, py, pw, ph = self.scale_coords(widths[0]-5, 0, 10, h)
+			self.drag_bars[0].update_position(px, py, pw, ph)
+			px, py, pw, ph = self.scale_coords(0, heights[0]-5, w, 10)
+			self.drag_bars[1].update_position(px, py, pw, ph)
+		elif layout == '5-2-3':
+			wcol = w // 2
+			h_left = [self.tiles[0].h, self.tiles[1].h]
+			h_right = [self.tiles[2].h, self.tiles[3].h, self.tiles[4].h]
+			for i in range(2):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i].h)
+				h_left[i] = ph
+			for i in range(3):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i+2].h)
+				h_right[i] = ph
+			# Left column
+			y_left = [0, h_left[0]]
+			for i in range(2):
+				px, py, pw, ph = self.scale_coords(0, (0 if i==0 else y_left[i-1]), wcol, h_left[i])
+				self.tiles[i].update_size(px, py, pw, ph)
+			# Right column
+			y_right = [0, h_right[0], h_right[0]+h_right[1]]
+			for i in range(3):
+				px, py, pw, ph = self.scale_coords(wcol, y_right[i], w-wcol, h_right[i])
+				self.tiles[i+2].update_size(px, py, pw, ph)
+			# Drag bars
+			# Left
+			px, py, pw, ph = self.scale_coords(0, h_left[0]-5, wcol, 10)
+			self.drag_bars_left[0].update_position(px, py, pw, ph)
+			# Right
+			for i in range(2):
+				px, py, pw, ph = self.scale_coords(wcol, h_right[i]-5+sum(h_right[:i]), w-wcol, 10)
+				self.drag_bars_right[i].update_position(px, py, pw, ph)
+		elif layout == '5-3-2':
+			wcol = w // 2
+			h_left = [self.tiles[0].h, self.tiles[1].h, self.tiles[2].h]
+			h_right = [self.tiles[3].h, self.tiles[4].h]
+			for i in range(3):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i].h)
+				h_left[i] = ph
+			for i in range(2):
+				_, _, _, ph = self.inv_scale_coords(0, 0, 0, self.tiles[i+3].h)
+				h_right[i] = ph
+			# Left column
+			y_left = [0]
+			for i in range(2):
+				y_left.append(y_left[-1] + h_left[i])
+			for i in range(3):
+				px, py, pw, ph = self.scale_coords(0, y_left[i], wcol, h_left[i])
+				self.tiles[i].update_size(px, py, pw, ph)
+			# Right column
+			y_right = [0, h_right[0]]
+			for i in range(2):
+				px, py, pw, ph = self.scale_coords(wcol, y_right[i], w-wcol, h_right[i])
+				self.tiles[i+3].update_size(px, py, pw, ph)
+			# Drag bars
+			for i in range(2):
+				px, py, pw, ph = self.scale_coords(0, h_left[i]-5+y_left[i], wcol, 10)
+				self.drag_bars_left[i].update_position(px, py, pw, ph)
+			px, py, pw, ph = self.scale_coords(wcol, h_right[0]-5, w-wcol, 10)
+			self.drag_bars_right[0].update_position(px, py, pw, ph)
 
 	def check_export(self):
 		self.images_imported = sum(1 for tile in self.tiles if tile.image_path)
